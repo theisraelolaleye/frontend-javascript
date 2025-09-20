5b14bee43e0dd836ef142991e4f59ba71c99b9a8
theisraelolaleye <theisraelolaleye@gmail.com>
2025-09-20 21:54:05 +0100

    11. Brand convention & Nominal typing

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,29 @@
+export interface MajorCredits {
+  credits: number;
+  _brand: "major";
+}
+
+export interface MinorCredits {
+  credits: number;
+  _brand: "minor";
+}
+
+export function sumMajorCredits(
+  subject1: MajorCredits,
+  subject2: MajorCredits
+): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    _brand: "major",
+  };
+}
+
+export function sumMinorCredits(
+  subject1: MinorCredits,
+  subject2: MinorCredits
+): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    _brand: "minor",
+  };
+}
